bcae3afe11bb7def370cf68e7a8d702686a79a97
tynan Sampsel <tynansampselr@gmail.com>
2023-12-07 19:47:39 -0400

    added bubble sort

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
+import { useState, useEffect } from "react";
+
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const [time, setTime] = useState(0);
+	const [mixedArray, setMixedArray] = useState([]);
+
+	useEffect(() => {
+		scrambleArray();
+	}, [])
+
+	//https://www.geeksforgeeks.org/shuffle-a-given-array-using-fisher-yates-shuffle-algorithm/
+	const scrambleArray = () => {
+		let newArr = Array(10000).fill(0);
+		for(let i = 0; i < newArr.length; i++){
+			newArr[i] = i;
+		}
+
+		for(let i = (newArr.length-1); i > 0; i--){
+
+			let j = Math.floor(Math.random() * (i+1));
+
+			[newArr[i], newArr[j]] = [newArr[j], newArr[i]]
+		}
+
+		console.log(newArr)
+		setMixedArray(newArr)
+	}
+
+	const sortA = () => {
+		const startTime = Date.now()
+
+		// for(let i = 0; i < 1000; i++){
+		// 	console.log("v")
+		// }
+
+		let mixedArrayCopy = [...mixedArray];
+		let isSorted = false;
+
+		while(!isSorted) {
+			let madeChanges = false;
+			for(let i = 0; i < mixedArrayCopy.length; i++){
+				if(i >= mixedArrayCopy.length) continue
+
+				if(mixedArrayCopy[i] > mixedArrayCopy[i+1]){
+
+					[mixedArrayCopy[i], mixedArrayCopy[i+1]] = [mixedArrayCopy[i+1], mixedArrayCopy[i]]
+
+					madeChanges = true;
+				}
+			}
+
+			if(!madeChanges) isSorted = true;
+		}
+
+		const endTime = Date.now()
+		setTime(endTime - startTime)
+		console.log(endTime - startTime)
+		console.log(mixedArrayCopy)
+	}
+
+
+	const generateRandom = () => {
+		
+	}
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>{time} miliseconds</h1>
+				<div onClick={sortA}>
+					run
+				</div>
+				<div onClick={generateRandom}>
+					generate
+				</div>
+			</header>
+		</div>
+	);
 }
 
 export default App;
