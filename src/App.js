01765b1706a7b61d8e0ad715e8df95032f7ad8a3
tynan Sampsel <tynansampselr@gmail.com>
2023-12-08 12:21:59 -0400

    merge sort started

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
 
 	//https://www.geeksforgeeks.org/shuffle-a-given-array-using-fisher-yates-shuffle-algorithm/
 	const scrambleArray = () => {
-		let newArr = Array(10000).fill(0);
+		let newArr = Array(10).fill(0);
 		for(let i = 0; i < newArr.length; i++){
 			newArr[i] = i;
 		}
@@ -63,6 +63,82 @@ function App() {
 	}
 
 
+	//does not work yet. 
+	const mergeSort = () => {
+		const startTime = Date.now()
+		let mixedArrayCopy = [...mixedArray];
+
+
+		const middle = Math.floor((mixedArrayCopy.length)/2);
+		const left = mixedArrayCopy.slice(0, middle)
+		const right = mixedArrayCopy.slice(middle, mixedArrayCopy.length)
+		console.log(mixedArrayCopy)
+
+		console.log("["+left+"] : ["+ right+"]")
+
+
+		const newLeft = rfunc(left);
+		const newRight = rfunc(right);
+
+
+		console.log("finished array")
+		console.log(newLeft+"  "+ newRight)
+
+		const newArr = newLeft[newLeft.length-1] < newRight[0] ? [...newLeft, ...newRight] : [...newRight, ...newLeft]
+
+		const endTime = Date.now()
+		setTime(endTime - startTime)
+		console.log(endTime - startTime)
+		console.log(newArr)
+	}
+
+	//takes 2 arrays, the low and high.
+	const rfunc = (arr) => {
+		console.log("r-------")
+		//console.log(arr)
+
+		if(arr.length <= 1){
+			console.log("["+arr+"] E")
+			return arr;
+		}
+
+		const middle = Math.floor((arr.length)/2);
+
+		//get half of left
+		const left = arr.slice(0, middle)
+		
+		//get half of right
+		const right = arr.slice(middle, arr.length)
+
+		console.log("["+left+"] : ["+ right+"] IN")
+
+
+		//recurse over left
+		//recurse over right
+		const newLeft = rfunc(left);
+		const newRight = rfunc(right);
+
+		//console.log("["+newLeft+"] : ["+ newRight+"] OUT")
+
+		if(newLeft[newLeft.length-1] < newRight[0]) {
+			console.log("["+newLeft+"] : ["+ newRight+"] OUT")
+
+			return [...newLeft, ...newRight]
+
+		} else {
+			console.log("["+newRight+"] : ["+ newLeft+"] OUT")
+
+			return [...newRight, ...newLeft]
+		}
+
+		//compare left to right and order and return
+		//return newLeft[newLeft.length-1] < newRight[0] ? [...newLeft, ...newRight] : [...newRight, ...newLeft]
+	}
+
+	//bucket
+
+	//merge
+
 	const generateRandom = () => {
 		
 	}
@@ -71,12 +147,22 @@ function App() {
 		<div className="App">
 			<header className="App-header">
 				<h1>{time} miliseconds</h1>
-				<div onClick={sortA}>
-					run
+				<div 
+					className="sortButton"
+					onClick={sortA}
+				>
+					bubble Sort
 				</div>
-				<div onClick={generateRandom}>
+
+				<div 
+					className="sortButton"
+					onClick={mergeSort}
+				>
+					Merge Sort
+				</div>
+				{/* <div onClick={generateRandom}>
 					generate
-				</div>
+				</div> */}
 			</header>
 		</div>
 	);
